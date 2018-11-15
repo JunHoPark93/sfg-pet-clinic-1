deee0f684e5b5d38c722e12560abde2ac7633cbc
pjh374968@gmail.com <pjh374968@gmail.com>
2018-11-15 15:54:59 +0900

    Add speciality, Add to vet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by jt on 7/13/18.
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialitties;
+
+    public Set<Speciality> getSpecialitties() {
+        return specialitties;
+    }
+
+    public void setSpecialitties(Set<Speciality> specialitties) {
+        this.specialitties = specialitties;
+    }
 }
