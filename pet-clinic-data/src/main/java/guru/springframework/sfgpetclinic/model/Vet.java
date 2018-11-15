e577602874f65dc2a25b44c042d85e2ac035cd47
pjh374968@gmail.com <pjh374968@gmail.com>
2018-11-15 20:38:52 +0900

    Create Specialities, add to Vets on startup (bootstrap)

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,13 +8,13 @@ import java.util.Set;
  */
 public class Vet extends Person {
 
-    private Set<Speciality> specialities;
+    private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialitties() {
+    public Set<Speciality> getSpecialities() {
         return specialities;
     }
 
-    public void setSpecialitties(Set<Speciality> specialities) {
+    public void setSpecialities(Set<Speciality> specialities) {
         this.specialities = specialities;
     }
 }
